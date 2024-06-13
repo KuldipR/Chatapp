c907a1deaa39ecc093c3370d13b90e0430adbc38
Kuldip Ranavaya <ranavayakuldip@gmail.com>
2024-06-12 23:22:33 -0700

    Solved issue and formatted code

--- index.js
+++ index.js
@@ -1,44 +1,43 @@
 // Node server which will handle socket io connections
-const express = require('express');
-const http = require('http');
-const path = require('path');
-
-
+const express = require("express");
+const http = require("http");
+const path = require("path");
 const app = express();
 const server = http.createServer(app);
-const io = require('socket.io')(server)
+const io = require("socket.io")(server);
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Serve index.html on the root route
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 const users = {};
 
-io.on('connection', socket =>{
-    // If any new user joins, let other users connected to the server know!
-    socket.on('new-user-joined', name =>{ 
-        users[socket.id] = name;
-        socket.broadcast.emit('user-joined', name);
+io.on("connection", (socket) => {
+  // If any new user joins, let other users connected to the server know!
+  socket.on("new-user-joined", (name) => {
+    users[socket.id] = name;
+    socket.broadcast.emit("user-joined", name);
+  });
+
+  // If someone sends a message, broadcast it to other people
+  socket.on("send", (message) => {
+    socket.broadcast.emit("receive", {
+      message: message,
+      name: users[socket.id],
     });
+  });
 
-    // If someone sends a message, broadcast it to other people
-    socket.on('send', message =>{
-        socket.broadcast.emit('receive', {message: message, name: users[socket.id]})
-    });
-
-    // If someone leaves the chat, let others know 
-    socket.on('disconnect', message =>{
-        socket.broadcast.emit('left', users[socket.id]);
-        delete users[socket.id];
-    });
-
-
-})
+  // If someone leaves the chat, let others know
+  socket.on("disconnect", (message) => {
+    socket.broadcast.emit("left", users[socket.id]);
+    delete users[socket.id];
+  });
+});
 
 const PORT = 3000;
 server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
--- public/client.js
+++ public/client.js
@@ -1,45 +1,39 @@
-
 const socket = io(`ws://localhost:3000`);
-
-console.log("Js in running")
-
-const form = document.getElementById('send');
+const form = document.getElementById("send");
 const messageInput = document.getElementById(`in`);
 const messageContainer = document.querySelector(`.container`);
-
 const append = (message, position) => {
-    const messageElement = document.createElement('div');
-    messageElement.innerText=message;
-    messageElement.classList.add('message')
-    messageElement.classList.add(position);
-    messageContainer.append(messageElement);
-}
-
+  const messageElement = document.createElement("div");
+  messageElement.innerText = message;
+  messageElement.classList.add("message");
+  messageElement.classList.add(position);
+  messageContainer.append(messageElement);
+};
 
 // Ask new user for his/her name and let the server know
 const name = prompt("Enter your name to join");
-socket.emit('new-user-joined', name);
+socket.emit("new-user-joined", name);
 
 // If a new user joins, receive his/her name from the server
-socket.on('user-joined', name =>{
-    append(`${name} joined the chat`, 'right')
-})
+socket.on("user-joined", (name) => {
+  append(`${name} joined the chat`, "right");
+});
 
 // If server sends a message, receive it
-socket.on('receive', data =>{
-    append(`${data.name}: ${data.message}`, 'left')
-})
+socket.on("receive", (data) => {
+  append(`${data.name}: ${data.message}`, "left");
+});
 
 // If a user leaves the chat, append the info to the container
-socket.on('left', name =>{
-    append(`${name} left the chat`, 'right')
-})
+socket.on("left", (name) => {
+  append(`${name} left the chat`, "right");
+});
 
 // If the form gets submitted, send server the message
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const message = messageInput.value;
-    append(`You: ${message}`, 'right');
-    socket.emit('send', message);
-    messageInput.value = ''
-})
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const message = messageInput.value;
+  append(`You: ${message}`, "right");
+  socket.emit("send", message);
+  messageInput.value = "";
+});
